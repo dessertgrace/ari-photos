f8ac1f94e92b242f29e66770c3d1eb61a1ec02d0
dessertgrace <dessertgrace@gmail.com>
2025-05-05 23:27:18 -0700

    github pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/ari' : '',
   images: {
-    domains: ['localhost'],
+    unoptimized: true,
   },
 }
 
